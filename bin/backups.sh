1322b84436afba23cebb9b4fe4cfcbc4a9499321
root <root@sergiomsi>
2018-01-10 16:16:38 +0100

    encriptacion

--- bin/backups.sh
+++ bin/backups.sh
@@ -2,7 +2,7 @@
 #
 #script para realizar backups completos,incrementales y diferenciales
 #
-#ejecucion: backup-bash.sh [full|incremental|diferencial] [remote]
+#ejecucion: backup-bash.sh full|incremental|diferencial [remote]
 
 ###	variables
 host=$(hostname)
@@ -11,13 +11,29 @@ BackupType="$1"
 date=$(date +%Y-%m-%d)
 logFile=$BackupLoc/logs/$host-$date.log
 snapBackup=$host.snar
-hostBackupDirs=/etc/backups/hosts/$host
+#hostBackupDirs=/etc/backups/hosts/$host
+hostBackupDirs=/home/sergio/github/backup-bash/etc/backups/hosts/$host
 remoteDir=/home/debian/backups
 remoteHost=172.22.200.225
 remoteUser=debian
 email=sergiotm87@gmail.com
 
+sergiomsi=172.22.5.111
+mickey=172.22.200
+minnie=172.22.81
+donald=172.22.85
+
+configfiles=/home/sergio/github/backup-bash/etc/backups/
+cryptloc=$BackupLoc/$host-$BackupType-$date #directorio donde se guardan los objetos encriptados
+
 ###	comprobaciones
+
+# numero argumentos correctos
+if [[ $# != 1 && $# != 2 ]]; then
+    echo "ERROR: Argumentos invalidos" | tee -a $logFile
+    echo "Uso: $0 [full|incremental|diferencial] [remote]" | tee -a $logFile
+    exit
+fi
 # directorio de backups
 if [ ! -d $BackupLoc ]; then
         echo "Creando Directorio: $BackupLoc"
@@ -39,10 +55,12 @@ fi
 function makeBackup(){
   echo "Ejecutando Backup" | tee -a $logFile
   mkdir $BackupLoc/$host-$BackupType-$date
-  tar --listed-incremental=$BackupLoc/$snapBackup -cvpzf $BackupLoc/$host-$BackupType-$date/$backupFile $(cat $hostBackupDirs | paste -sd " " -) 2>> $logFile 1>> $logFile
+  encriptar
+  tar --listed-incremental=$BackupLoc/$snapBackup -cpzf $BackupLoc/$host-$BackupType-$date/$backupFile $(awk '{if ($1 =="NC") print $2 }' $hostBackupDirs | paste -sd " " -) 2>> $logFile 1>> $logFile
   if [[ $? = 0 ]]; then
           echo "Backup realizado: $backupFile" | tee -a $logFile
           date > $BackupLoc/lastbackup.txt | tee -a $logFile
+          #insercionCoconut
   else
           echo "Backup no realizado" | tee -a $logFile
           mail -s "error de backup" $email < $logFile
@@ -50,10 +68,42 @@ function makeBackup(){
   fi
 }
 
-
 function insercionCoconut(){
-  echo "Insercion en Coconut" | tee -a $logFile
+  echo "Insercion en Coconut.." | tee -a $logFile
+  status=200
+  psql -h 172.22.200.110 -U Sergio.Teran -d db_backup -c "INSERT INTO BACKUPS (backup_user, backup_host, backup_label, backup_description, backup_status, backup_mode) values ('Sergio.Teran', '${!host}','$backupFile','Copia $1 de $host', '$status', 'Automatica')"
+  if [[ $? = 0 ]]; then
+          echo "Insercion realizada: $backupFile" | tee -a $logFile
+  else
+          echo "Insercion no realizado" | tee -a $logFile
+          mail -s "error de insercion en Coconut" $email < $logFile
+          exit 1
+  fi
 }
+
+function encriptar(){
+  echo "Encriptando ficheros marcados" | tee -a $logFile
+  openssl rand -out ${cryptloc}/key.txt -base64 48
+  while read line; do
+    option=$(echo ${line} | cut -d' ' -f1)
+    fichero=$(echo ${line} | cut -d' ' -f2)
+    ficherotar=${fichero}.tar.gz
+    if [[ ${option} == 'C' ]]; then
+      tar -cpzf ${ficherotar} ${fichero} &>/dev/null
+      openssl enc -aes-256-cbc -pass file:${cryptloc}/key.txt -in ${ficherotar} -out ${cryptloc}${ficherotar}.encrypted
+      rm -rf ${ficherotar}
+    fi
+  done <${hostBackupDirs}
+  openssl rsautl -encrypt -in ${cryptloc}/key.txt -out ${cryptloc}/key.encrypted -inkey ${configfiles}backup.pub.pem -pubin
+  rm ${cryptloc}/key.txt
+}
+
+
+# function desencriptar(){
+#   echo "Desencriptando directorio" | tee -a $logFile
+#   openssl rsautl -decrypt -inkey ./backup.pem -in key.encrypted -out key.txt
+#   openssl enc -aes-256-cbc -pass file:key.txt -in $objeto -out $(echo $objeto | cut -d'.' -f 1,2,3)
+# }
 
 ###	full backup
 # genera una copia completa y un snapshot sobre el que realizar las copias incrementales
@@ -85,19 +135,19 @@ fi
 ###   diferencial backup
 # genera copias diferenciales desde la fecha del ultimo backup completo/incremental
 
-if [ "$BackupType" == "diferencial" ]; then
-        backupFile=$(hostname)-$date-diferencial.tar.gz
-        mkdir $BackupLoc/$host-$BackupType-$date
-        echo "Tipo de backup: DIFERENCIAL" | tee -a $logFile
-        tar -cvpzf $BackupLoc/$host-$BackupType-$date/$backupFile -N $BackupLoc/lastbackup.txt $(cat $hostBackupDirs | paste -sd " " -) 2>> $logFile 1>> $logFile
-        if [[ $? = 0 ]]; then
-                echo "Backup realizado: $backupFile" | tee -a $logFile
-        else
-                echo "Backup no realizado" | tee -a $logFile
-                mail -s "error de backup" $email < $logFile
-                exit 1
-        fi
-fi
+# if [ "$BackupType" == "diferencial" ]; then
+#         backupFile=$(hostname)-$date-diferencial.tar.gz
+#         mkdir $BackupLoc/$host-$BackupType-$date
+#         echo "Tipo de backup: DIFERENCIAL" | tee -a $logFile
+#         tar -cvpzf $BackupLoc/$host-$BackupType-$date/$backupFile -N $BackupLoc/lastbackup.txt $(cat $hostBackupDirs | paste -sd " " -) 2>> $logFile 1>> $logFile
+#         if [[ $? = 0 ]]; then
+#                 echo "Backup realizado: $backupFile" | tee -a $logFile
+#         else
+#                 echo "Backup no realizado" | tee -a $logFile
+#                 mail -s "error de backup" $email < $logFile
+#                 exit 1
+#         fi
+# fi
 
 ###	rsync
 
@@ -115,3 +165,12 @@ if [ "$2" == "remote" ]; then
                 mail -s "error de backup" $email < $logFile
         fi
 fi
+
+
+## todo
+# terminar desencriptar (y ajustar el cryptloc de los ficheros a la ruta correspondiente)
+# cambiar a diferencial sobre completas, incrementales sobre completas o diferenciales
+# poner hostBackupDirs-configfiles correcto
+# documentar fichero ~/.pgpass (chmod 0600 ~/.pgpass) para la insercion en coconut
+# probar en mickey,minnie,donald
+# https://help.ubuntu.com/community/BackupYourSystem/TAR
